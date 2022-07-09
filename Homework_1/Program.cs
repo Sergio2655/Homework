c5c1529162d208a3d49b071ab5f15860dfab0d6d
Sergey <sp27101971@gmail.com>
2022-07-09 16:56:00 +0300

    Largest number from two

---
+++ Homework_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Enter first number: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int y = int.Parse(Console.ReadLine());
+if(x > y)
+{
+    Console.WriteLine("The largest number is " + x);
+}
+else
+{
+    Console.WriteLine("The largest number is " + y);
+}
