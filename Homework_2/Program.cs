abcf5196e4caf6f2d181e0937d428fc99c124ace
Sergey <sp27101971@gmail.com>
2022-07-09 18:00:52 +0300

    Finita

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -4,3 +4,13 @@
 22 3 9 -> 22
 */
 
+Console.WriteLine("Enter first number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter third number");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("The largest number is "+ max);
