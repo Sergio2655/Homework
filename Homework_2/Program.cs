7a75442b1d99854651217840bb37163ce3d2c3e5
Sergey <sp27101971@gmail.com>
2022-07-10 18:33:02 +0300

    Largest number of three

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -4,11 +4,11 @@
 22 3 9 -> 22
 */
 
-Console.WriteLine("Enter first number");
+Console.WriteLine("Please, enter your first number:");
 int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter second number");
+Console.WriteLine("Please, enter your second number:");
 int b = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter third number");
+Console.WriteLine("Please, enter your third number:");
 int c = int.Parse(Console.ReadLine());
 int max = a;
 if (b > max) max = b;
