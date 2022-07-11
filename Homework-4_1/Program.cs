3c3977a621facc62937579f408dcc7eccd8600c6
Sergey <sp27101971@gmail.com>
2022-07-11 22:25:07 +0300

    even numbers in right order

---
+++ Homework-4_1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter number:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("+++++++++++++++++");
+Console.WriteLine("Got it:)");
+Console.WriteLine("Here we go!");
+Console.WriteLine("+++++++++++++++++");
+int a = 2;
+if(n % 2 == 0)
+{
+    while(a < n)
+    {
+        Console.WriteLine(a);
+        a = a + 2;
+    }
+    Console.WriteLine(n);
+}
+else
+{
+    while(a < n)
+    {
+        Console.WriteLine(a);
+        a = a + 2;
+    }
+}
