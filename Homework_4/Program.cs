c0259c1c9efb972f14dc205afbc5b7a210bc54eb
Sergey <sp27101971@gmail.com>
2022-07-11 19:18:35 +0300

    Even numbers

---
+++ Homework_4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает
+ все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.WriteLine("Please, enter your number:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Got it)");
+Console.WriteLine("Here we go");
+if(n % 2 ==0)
+{
+    while(n > 1)
+    {
+    Console.WriteLine(n);
+    n = n - 2;
+    }
+}
+else
+{
+    n = n - 1;
+    while(n > 1)
+    {
+    Console.WriteLine(n);
+    n = n - 2;
+    }
+}
